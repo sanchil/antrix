6fb123631fe7b5fa84a43f4706323e2b614cc22e
azure <sandeepnet@aol.com>
2020-08-18 16:49:38 -0400

    18aug2020_1

---
+++ src/consts/antrixtek.js
@@ -0,0 +1,14 @@
+
+export const db= 'antrixdb';
+export const dburl= 'https://antrixtek.com/db/'+db;
+export const dbuid= 'admin';
+export const dbpwd= 'admin';
+
+const CONST = {
+    db,
+    dburl,
+    dbuid,
+    dbpwd
+}
+
+export default CONST;
